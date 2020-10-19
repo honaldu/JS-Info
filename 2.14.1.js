7b88976d7a52e35838bcbd4df4c1cc2268aae09e
허석호 <microcephalus7@gmail.com>
2020-10-19 19:42:35 +0900

    switch, case

---
+++ 2.14.1.js
@@ -0,0 +1,12 @@
+if (browser === "edge") {
+  alert("Edge ㅎㅇ");
+} else if (
+  browser === "Chrome" ||
+  browser === "Firefox" ||
+  browser === "Safari" ||
+  browser === "Opera"
+) {
+  alert("지원중임!");
+} else {
+  alert("와! 처음 보는 브라우저!!! 충격!!");
+}
